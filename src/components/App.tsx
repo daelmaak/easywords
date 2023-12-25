d1195e9ad5ca3b53eba5d89dea7becebfc141503
Daniel Macak <daniel.macak@email.cz>
2023-12-25 17:26:28 +0100

    change: create a primitive router
    Since I plan to add conjugations I need a different view for that. So
    far I am using a primite router so that I don't commit into a lib that I
    might not need eventually.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,102 +1,26 @@
-import { get, set } from 'idb-keyval';
-import { Show, createEffect, createSignal, type Component } from 'solid-js';
-import { WordTranslation } from '../parser/simple-md-parser';
-import { Config } from './Config';
-import { Results } from './Results';
-import Tester, { TestMode } from './Tester';
-import { WordsInput } from './WordsInput';
+import { Component, createSignal } from 'solid-js';
+import VocabularyView from './VocabularyView';
+
+type View = 'vocabulary' | 'conjugations';
 
 const App: Component = () => {
-  const [lastWords, setLastWords] = createSignal<WordTranslation[]>();
-  const [words, setWords] = createSignal<WordTranslation[]>();
-  const [mode, setMode] = createSignal<TestMode>('write');
-  const [reverse, setReverse] = createSignal(false);
-  const [invalidWords, setInvalidWords] = createSignal<WordTranslation[]>();
-  const [done, setDone] = createSignal(false);
+  const [currentView, setCurrentView] = createSignal<View>('vocabulary');
 
-  createEffect(async () => {
-    setLastWords(await get<WordTranslation[]>('last-words'));
-  });
-
-  async function onDone(leftOverWords?: WordTranslation[]) {
-    setDone(true);
-    setInvalidWords(leftOverWords);
-    await storeWords(leftOverWords);
-  }
-
-  function onRepeat() {
-    setInvalidWords();
-    setDone(false);
-  }
-
-  function onReset() {
-    setInvalidWords();
-    setWords();
-    setDone(false);
-  }
-
-  function onTryInvalidWords(invalidWords: WordTranslation[]) {
-    setInvalidWords();
-    setWords(invalidWords);
-    setDone(false);
-  }
-
-  async function selectWords(words: WordTranslation[]) {
-    setInvalidWords();
-    setWords(words);
-    await storeWords(words);
-  }
-
-  async function storeWords(words?: WordTranslation[]) {
-    await set('last-words', words);
-  }
+  const switchView = (view: View) => () => setCurrentView(view);
 
   return (
-    <div class="min-h-full grid p-8 bg-zinc-800">
-      <div class="m-auto">
-        <Show when={!words() && !done()}>
-          <WordsInput
-            onWordsSelect={selectWords}
-            reverse={reverse()}
-            storedWords={lastWords()}
-          />
-        </Show>
-
-        <Show when={!done()}>
-          <Show keyed={true} when={words()}>
-            {w => (
-              <Tester
-                mode={mode()}
-                reverse={reverse()}
-                words={w}
-                done={onDone}
-                repeat={onRepeat}
-                reset={onReset}
-              />
-            )}
-          </Show>
-        </Show>
-
-        <Show when={done()}>
-          <Results
-            invalidWords={invalidWords()}
-            repeat={onRepeat}
-            reset={onReset}
-            tryInvalidWords={onTryInvalidWords}
-          />
-        </Show>
-
-        <Config modeChange={setMode} reverseTranslations={setReverse} />
-      </div>
-
-      <Show when={words()}>
-        <button
-          class="btn-link fixed bottom-4 right-8 text-sm"
-          onClick={onReset}
-        >
-          Go back
-        </button>
-      </Show>
+    <div class="min-h-full p-8 bg-zinc-800 flex flex-col">
+      <nav class="flex gap-4">
+        <a class="cursor-pointer" onClick={switchView('vocabulary')}>
+          Vocabulary
+        </a>
+        <a class="cursor-pointer" onClick={switchView('conjugations')}>
+          Conjugations
+        </a>
+      </nav>
+      <main class="m-auto grid">
+        {currentView() === 'vocabulary' ? <VocabularyView /> : null}
+      </main>
     </div>
   );
 };
---
+++ src/components/VocabularyView.tsx
@@ -0,0 +1,102 @@
+import { get, set } from 'idb-keyval';
+import { Show, createEffect, createSignal, type Component } from 'solid-js';
+import { WordTranslation } from '../parser/simple-md-parser';
+import { Config } from './Config';
+import { Results } from './Results';
+import Tester, { TestMode } from './Tester';
+import { WordsInput } from './WordsInput';
+
+const App: Component = () => {
+  const [lastWords, setLastWords] = createSignal<WordTranslation[]>();
+  const [words, setWords] = createSignal<WordTranslation[]>();
+  const [mode, setMode] = createSignal<TestMode>('write');
+  const [reverse, setReverse] = createSignal(false);
+  const [invalidWords, setInvalidWords] = createSignal<WordTranslation[]>();
+  const [done, setDone] = createSignal(false);
+
+  createEffect(async () => {
+    setLastWords(await get<WordTranslation[]>('last-words'));
+  });
+
+  async function onDone(leftOverWords?: WordTranslation[]) {
+    setDone(true);
+    setInvalidWords(leftOverWords);
+    await storeWords(leftOverWords);
+  }
+
+  function onRepeat() {
+    setInvalidWords();
+    setDone(false);
+  }
+
+  function onReset() {
+    setInvalidWords();
+    setWords();
+    setDone(false);
+  }
+
+  function onTryInvalidWords(invalidWords: WordTranslation[]) {
+    setInvalidWords();
+    setWords(invalidWords);
+    setDone(false);
+  }
+
+  async function selectWords(words: WordTranslation[]) {
+    setInvalidWords();
+    setWords(words);
+    await storeWords(words);
+  }
+
+  async function storeWords(words?: WordTranslation[]) {
+    await set('last-words', words);
+  }
+
+  return (
+    <>
+      <Show when={!words() && !done()}>
+        <WordsInput
+          onWordsSelect={selectWords}
+          reverse={reverse()}
+          storedWords={lastWords()}
+        />
+      </Show>
+
+      <Show when={!done()}>
+        <Show keyed={true} when={words()}>
+          {w => (
+            <Tester
+              mode={mode()}
+              reverse={reverse()}
+              words={w}
+              done={onDone}
+              repeat={onRepeat}
+              reset={onReset}
+            />
+          )}
+        </Show>
+      </Show>
+
+      <Show when={done()}>
+        <Results
+          invalidWords={invalidWords()}
+          repeat={onRepeat}
+          reset={onReset}
+          tryInvalidWords={onTryInvalidWords}
+        />
+      </Show>
+
+      <Config modeChange={setMode} reverseTranslations={setReverse} />
+
+      <Show when={words()}>
+        <button
+          class="btn-link fixed bottom-4 right-8 text-sm"
+          onClick={onReset}
+        >
+          Go back
+        </button>
+      </Show>
+    </>
+  );
+};
+
+export default App;
