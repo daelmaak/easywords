7cef8dd42810ea9acb5667835002346b5417fa68
Daniel Macak <daniel.macak@email.cz>
2023-12-29 00:20:50 +0100

    add: checkbox filter component
    The rationale behind why checkbox and not chips filter is that chips
    work only as part of one category, but the tenses have to be
    categorized by multiple moods and as such I need hierarchy with
    checkboxes.

--- src/domains/conjugations/conjugation.ts
+++ src/domains/conjugations/conjugation.ts
@@ -1,4 +1,6 @@
 export interface Conjugation {
+  mood: string;
+  tense: string;
   person: string;
   conjugatedVerb: string;
 }
@@ -10,4 +12,46 @@ export interface ConjugationByTense {
 
 export interface ConjugationsByTense {
   [tense: string]: Conjugation[];
+}
+
+export interface ConjugationsByMood {
+  [mood: string]: ConjugationByTense[];
+}
+
+export function groupConjugationsByTense(
+  conjugations: Conjugation[]
+): ConjugationsByTense {
+  return conjugations.reduce((acc, conj) => {
+    const { tense } = conj;
+
+    if (!acc[tense]) {
+      acc[tense] = [];
+    }
+
+    acc[tense].push(conj);
+
+    return acc;
+  }, {} as ConjugationsByTense);
+}
+
+export function groupConjugationsByMood(
+  conjugations: Conjugation[]
+): ConjugationsByMood {
+  return conjugations.reduce((acc, conj) => {
+    const { mood } = conj;
+
+    if (!acc[mood]) {
+      acc[mood] = [];
+    }
+
+    const tense = acc[mood].find(t => t.tense === conj.tense);
+
+    if (!tense) {
+      acc[mood].push({ tense: conj.tense, conjugations: [conj] });
+    } else {
+      tense.conjugations.push(conj);
+    }
+
+    return acc;
+  }, {} as ConjugationsByMood);
 }
