425807e8dad816b250bc4f933b445a3274187e5e
Daniel Macak <daniel.macak@email.cz>
2024-01-17 23:21:39 +0100

    add: display '-' when conjugation answer missing

--- src/domains/conjugations/components/ConjugationsResults.tsx
+++ src/domains/conjugations/components/ConjugationsResults.tsx
@@ -44,7 +44,7 @@ export const ConjugationsResults: Component<Props> = props => {
                             'text-red-500': !conjugationResult.valid,
                           }}
                         >
-                          {conjugationResult.answer}
+                          {conjugationResult.answer || '-'}
                         </td>
                       </tr>
                     )}
