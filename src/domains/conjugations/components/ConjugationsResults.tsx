c3dd8898659909892b0b1585f6109b71012d5ca0
Daniel Macak <daniel.macak@email.cz>
2024-01-17 22:53:16 +0100

    add: basic conjugation styling
    Let's be honest, I am no designer :). But I gave the results some
    structure and at least made them more readable.

--- src/domains/conjugations/components/ConjugationsResults.tsx
+++ src/domains/conjugations/components/ConjugationsResults.tsx
@@ -13,27 +13,39 @@ interface Props {
 export const ConjugationsResults: Component<Props> = props => {
   return (
     <div>
-      <h2>Results</h2>
-      <div class="flex gap-8">
+      <h2 class="text-center text-2xl font-semibold">Results</h2>
+      <div class="flex flex-wrap gap-8">
         <For each={Object.keys(props.conjugationsResults)}>
           {tense => (
-            <div>
-              <h3>{tense}</h3>
-              <table>
-                <thead>
+            <div class="mt-6 mx-auto">
+              <h3 class="text-center text-lg text-zinc-300">{tense}</h3>
+              <table class="mt-2 border-separate border-spacing-x-4 border-spacing-y-2">
+                <thead class="text-left text-zinc-400">
                   <tr>
-                    <th>Person</th>
-                    <th>Expected</th>
-                    <th>Actual</th>
+                    <th></th>
+                    <th>Correct</th>
+                    <th>Yours</th>
                   </tr>
                 </thead>
                 <tbody>
                   <For each={props.conjugationsResults[tense]}>
                     {conjugationResult => (
                       <tr>
-                        <td>{conjugationResult.conjugation.person}</td>
-                        <td>{conjugationResult.conjugation.conjugatedVerb}</td>
-                        <td>{conjugationResult.answer}</td>
+                        <td class="border-r border-r-zinc-500 pr-8 text-right">
+                          {conjugationResult.conjugation.person}
+                        </td>
+                        <td class="border-r border-r-zinc-500 pr-8 text-right">
+                          {conjugationResult.conjugation.conjugatedVerb}
+                        </td>
+                        <td
+                          class="pr-8"
+                          classList={{
+                            'text-green-600': conjugationResult.valid,
+                            'text-red-500': !conjugationResult.valid,
+                          }}
+                        >
+                          {conjugationResult.answer}
+                        </td>
                       </tr>
                     )}
                   </For>
@@ -43,7 +55,7 @@ export const ConjugationsResults: Component<Props> = props => {
           )}
         </For>
       </div>
-      <div class="mt-4 flex gap-2">
+      <div class="mt-12 flex justify-center gap-2">
         <Button style="secondary" onClick={props.onTryAgain}>
           Try again
         </Button>
