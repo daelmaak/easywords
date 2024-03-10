c61ebefc37784b194734aab03dcaeece66214450
Daniel Macak <daniel.macak@email.cz>
2024-03-10 12:56:46 +0100

    change: simplify the element structure
    Remove unnecessary DOM nesting.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -38,36 +38,32 @@ const App: Component<Props> = props => {
 
   return (
     <LangContext.Provider value={currentLang}>
-      <div class="min-h-full py-4 px-8 dark:bg-zinc-800 flex flex-col">
-        <nav class="flex items-center">
-          <A href="/vocabulary">
-            <Button class="text-inherit" variant="link">
-              Vocabulary
-            </Button>
-          </A>
-          <A href="/conjugations">
-            <Button class="text-inherit" variant="link">
-              Conjugations
-            </Button>
-          </A>
-          <AccountButton
-            loggedIn={!!loggedIn()}
-            onSignIn={onSignIn}
-            onSignOut={onSignOut}
-            onSignUp={onSignUp}
-          />
-          <select
-            class="select"
-            onChange={e => changeLang(e.currentTarget.value)}
-            value={currentLang()}
-          >
-            <For each={langs}>
-              {lang => <option value={lang}>{lang}</option>}
-            </For>
-          </select>
-        </nav>
-        <main class="flex-grow w-full py-8 grid">{props.children}</main>
-      </div>
+      <nav class="flex items-center">
+        <A href="/vocabulary">
+          <Button class="text-inherit" variant="link">
+            Vocabulary
+          </Button>
+        </A>
+        <A href="/conjugations">
+          <Button class="text-inherit" variant="link">
+            Conjugations
+          </Button>
+        </A>
+        <AccountButton
+          loggedIn={!!loggedIn()}
+          onSignIn={onSignIn}
+          onSignOut={onSignOut}
+          onSignUp={onSignUp}
+        />
+        <select
+          class="select"
+          onChange={e => changeLang(e.currentTarget.value)}
+          value={currentLang()}
+        >
+          <For each={langs}>{lang => <option value={lang}>{lang}</option>}</For>
+        </select>
+      </nav>
+      <main class="flex-grow w-full py-8 grid">{props.children}</main>
     </LangContext.Provider>
   );
 };
