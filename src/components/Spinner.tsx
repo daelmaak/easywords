b0fc5aceee935e00bc7e370e83d3a571656cd88c
Daniel Macak <daniel.macak@email.cz>
2024-03-25 13:26:12 +0100

    change: get rid of the old button
    I still use the old spinner though since it's such a nice animation.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,25 +1,24 @@
 import { Component } from 'solid-js';
+import style from './Spinner.module.css';
+import { cn } from '~/lib/utils';
 
 interface Props {
-  class?: string;
   size: 'md' | 'lg';
 }
 
 export const Spinner: Component<Props> = props => {
-  const widthClass = () => (props.size === 'md' ? 'w-6' : 'w-8');
-  const heightClass = () => (props.size === 'md' ? 'h-6' : 'h-8');
+  const sizeClass = () => (props.size === 'md' ? 'size-6' : 'size-8');
 
   return (
-    <svg
-      class="spinner"
-      classList={{
-        [props.class ?? '']: true,
-        [widthClass()]: true,
-        [heightClass()]: true,
-      }}
-      viewBox="0 0 50 50"
-    >
-      <circle cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+    <svg class={cn('animate-spin', sizeClass())} viewBox="0 0 50 50">
+      <circle
+        class={style.circle}
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        stroke-width="5"
+      ></circle>
     </svg>
   );
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,49 +1,77 @@
-import type { Component, ComponentProps } from "solid-js"
-import { splitProps } from "solid-js"
+import type { Component, ComponentProps } from 'solid-js';
+import { splitProps } from 'solid-js';
 
-import type { VariantProps } from "class-variance-authority"
-import { cva } from "class-variance-authority"
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
 
-import { cn } from "~/lib/utils"
+import { cn } from '~/lib/utils';
+import { Spinner } from '../Spinner';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border border-input hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline"
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline:
+          'border border-input hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "size-10"
-      }
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'size-10',
+      },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default"
-    }
+      variant: 'default',
+      size: 'default',
+    },
   }
-)
+);
 
 export interface ButtonProps
-  extends ComponentProps<"button">,
-    VariantProps<typeof buttonVariants> {}
-
-const Button: Component<ButtonProps> = (props) => {
-  const [, rest] = splitProps(props, ["variant", "size", "class"])
-  return (
-    <button
-      class={cn(buttonVariants({ variant: props.variant, size: props.size }), props.class)}
-      {...rest}
-    />
-  )
+  extends ComponentProps<'button'>,
+    VariantProps<typeof buttonVariants> {
+  loading?: boolean;
 }
 
-export { Button, buttonVariants }
+const Button: Component<ButtonProps> = props => {
+  const [_, local, rest] = splitProps(
+    props,
+    ['variant', 'size', 'class'],
+    ['children', 'loading']
+  );
+  return (
+    <button
+      class={cn(
+        buttonVariants({ variant: props.variant, size: props.size }),
+        props.class,
+        'relative'
+      )}
+      {...rest}
+    >
+      {local.loading && (
+        <span class="absolute left-1/2 -translate-x-1/2">
+          <Spinner size={'md'} />
+        </span>
+      )}
+      <span
+        class="inline-flex items-end gap-1"
+        classList={{
+          invisible: local.loading,
+        }}
+      >
+        {local.children}
+      </span>
+    </button>
+  );
+};
+
+export { Button, buttonVariants };
