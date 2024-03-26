f717e321bb2ed104a2fc220ef27c7d108411dc27
Daniel Macak <daniel.macak@email.cz>
2024-03-26 17:43:44 +0100

    chore: get rid of old checkbox
    Unifying the UI by using the solid-ui's checkbox made the old Checkbox
    superfluous.
    
    One interesting thing is that the generated checkbox didn't include any
    label. That responsibility lied with the user, but I find this
    impractical, so I put it in. Let's see how it fares. If only in some
    cases I'd need custom label, I can still hide the build-in one and use a
    custom one.

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,31 @@
+import type { Component } from 'solid-js';
+import { splitProps } from 'solid-js';
+
+import { Checkbox as CheckboxPrimitive } from '@kobalte/core';
+import { TbCheck } from 'solid-icons/tb';
+
+import { cn } from '~/lib/utils';
+
+const Checkbox: Component<
+  CheckboxPrimitive.CheckboxRootProps & { label: string }
+> = props => {
+  const [, local, rest] = splitProps(props, ['class'], ['label']);
+  return (
+    <CheckboxPrimitive.Root
+      class={cn('items-center group flex space-x-2', props.class)}
+      {...rest}
+    >
+      <CheckboxPrimitive.Input />
+      <CheckboxPrimitive.Control class="mt-0.5 peer size-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:border-none data-[checked]:bg-primary data-[checked]:text-primary-foreground cursor-pointer">
+        <CheckboxPrimitive.Indicator>
+          <TbCheck class="size-4" />
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Control>
+      <CheckboxPrimitive.Label id={`${rest.id}-input`} class="cursor-pointer">
+        {local.label}
+      </CheckboxPrimitive.Label>
+    </CheckboxPrimitive.Root>
+  );
+};
+
+export { Checkbox };
