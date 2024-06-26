b87810a9160e6870b4b58e11262d5dae0808f64d
Daniel Macak <daniel.macak@email.cz>
2024-06-26 22:40:54 +0200

    fix: eslint errors

--- src/util/form.ts
+++ src/util/form.ts
@@ -2,7 +2,7 @@ export function processFormSubmit<T extends object>(event: SubmitEvent): T {
   event.preventDefault();
 
   const formData = new FormData(event.target as HTMLFormElement);
-  const data: any = {};
+  const data: Record<string, FormDataEntryValue> = {};
 
   formData.forEach((value, key) => {
     data[key] = value;
