ce78423d011a1d3b273d567a6186f44b923fe311
Daniel Macak <daniel.macak@email.cz>
2024-07-02 09:43:38 +0200

    add: new auth page
    Since I abandonned the concept of signing up in the last moment, I have
    to have a proper auth page now. I combined both the login and signup
    together for now but this can change if I diversify the content more.

---
+++ src/domains/auth/AuthPage.tsx
@@ -0,0 +1,54 @@
+import { Show, createSignal } from 'solid-js';
+import { AuthMode } from './model';
+import { useLocation } from '@solidjs/router';
+import { AuthForm } from './AuthForm';
+import logo from '../../assets/logo.svg';
+
+export const AuthPage = () => {
+  const location = useLocation();
+  const isLogin = location.pathname.endsWith('login');
+
+  const [mode, setMode] = createSignal<AuthMode>(isLogin ? 'signin' : 'signup');
+
+  return (
+    <div class="h-full w-full bg-gray-200 grid grid-rows-[4rem_auto_6rem]">
+      <div class="m-auto flex items-center gap-1">
+        <img src={logo} alt="logo" class="size-8 mb-1" />
+        <span class="text-2xl font-semibold">Easywords</span>
+      </div>
+
+      <div class="m-auto min-w-80 ">
+        <Show when={mode() === 'signin'}>
+          <h1
+            class="mb-8 text-3xl font-semibold text-center"
+            aria-label="Sign into Easywords"
+          >
+            Welcome back!
+          </h1>
+        </Show>
+        <Show when={mode() === 'signup'}>
+          <h1
+            class="mb-8 text-3xl font-semibold text-center"
+            aria-label="Sign up to Easywords"
+          >
+            Welcome to Easywords!
+          </h1>
+        </Show>
+
+        <div class="bg-white rounded-xl p-4 sm:min-w-96 sm:p-6">
+          <Show when={mode() === 'signin'}>
+            <h2 class="text-lg font-semibold text-center">
+              Log into your account
+            </h2>
+          </Show>
+          <Show when={mode() === 'signup'}>
+            <h2 class="text-lg font-semibold text-center">Create an account</h2>
+          </Show>
+          <AuthForm mode={mode()} onModeChange={setMode} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
--- src/domains/auth/LoginPage.tsx
+++
@@ -1,7 +0,0 @@
-import { AuthForm } from './AuthForm';
-
-export const LoginPage = () => {
-  return <AuthForm mode="signin" onModeChange={() => {}} />;
-};
-
-export default LoginPage;
