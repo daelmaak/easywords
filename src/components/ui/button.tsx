29ce9747893608a68d11ce08bf3d78df64171b34
Daniel Macak <damacak@cisco.com>
2024-07-14 16:07:39 +0200

    fix: align icons in buttons to the same level
    Classic icon button text misalignment issue fixed.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,6 +7,8 @@ import { cva } from 'class-variance-authority';
 import { cn } from '~/lib/utils';
 import { Spinner } from '../Spinner';
 
+import classes from './button.module.css';
+
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
@@ -53,6 +55,7 @@ const Button: Component<ButtonProps> = props => {
       class={cn(
         buttonVariants({ variant: props.variant, size: props.size }),
         props.class,
+        classes.button,
         'relative'
       )}
       disabled={local.loading}
