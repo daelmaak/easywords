0bc6033c755e86a2ab1b2bdf0da498503d1c3b78
Daniel Macak <daniel.macak@email.cz>
2024-08-10 19:39:51 +0200

    add: pie chart for results
    Ofc I am using 3rd party for this - chart.js.
    
    I removed the legend since it was needlessly taking up a lot of space
    and will add my own the future.
    
    I also fixed couple of bugs which were causing invalid words section to
    be shown even if all results were correct.
    
    For the future, I'd like to add some confetti, motivational feedback and
    find a way to visualize the number of attempts for each word.

--- src/domains/vocabulary-results/components/VocabularyResultsMini.tsx
+++ src/domains/vocabulary-results/components/VocabularyResultsMini.tsx
@@ -1,13 +1,18 @@
-import { type Component } from 'solid-js';
+import { Show, type Component } from 'solid-js';
 import type { TestResult } from '../model/test-result-model';
 import { Card, CardContent } from '~/components/ui/card';
-import { TestResultsVisualisation } from './TestResultsVisualisation';
 
 interface Props {
   result: TestResult;
 }
 
 export const VocabularyResultsMini: Component<Props> = props => {
+  const correctAnswers = () =>
+    props.result.words.filter(w => w.done && w.invalidAttempts === 0).length;
+  const incorrectAnswers = () =>
+    props.result.words.filter(w => w.invalidAttempts > 0).length;
+  const skippedAnswers = () => props.result.words.filter(w => w.skipped).length;
+
   return (
     <Card class="mt-8">
       <CardContent class="p-4">
@@ -17,7 +22,46 @@ export const VocabularyResultsMini: Component<Props> = props => {
             {props.result.updatedAt.toLocaleDateString()}
           </span>
         </div>
-        <TestResultsVisualisation result={props.result} />
+        <figure class="mt-2">
+          <figcaption>
+            <dl class="text-sm font-semibold">
+              <div class="flex gap-1 text-green-900">
+                <dd class="">{correctAnswers().toFixed(0)}</dd>
+                <dt>correct</dt>
+              </div>
+              <div class="flex gap-1 text-red-900">
+                <dd class="">{incorrectAnswers().toFixed(0)}</dd>
+                <dt>incorrect</dt>
+              </div>
+              <Show when={+skippedAnswers() > 0}>
+                <div class="flex gap-1 text-gray-700">
+                  <dd class="">{skippedAnswers().toFixed(0)}</dd>
+                  <dt>skipped</dt>
+                </div>
+              </Show>
+            </dl>
+          </figcaption>
+          <div class="mt-2 w-full flex gap-[2px] rounded-sm overflow-hidden">
+            <Show when={correctAnswers() > 0}>
+              <div
+                class="h-2 bg-green-900"
+                style={{ 'flex-grow': correctAnswers() }}
+              ></div>
+            </Show>
+            <Show when={incorrectAnswers() > 0}>
+              <div
+                class="h-2 bg-red-700"
+                style={{ 'flex-grow': incorrectAnswers() }}
+              ></div>
+            </Show>
+            <Show when={skippedAnswers() > 0}>
+              <div
+                class="h-2 bg-gray-400"
+                style={{ 'flex-grow': skippedAnswers() }}
+              ></div>
+            </Show>
+          </div>
+        </figure>
       </CardContent>
     </Card>
   );
