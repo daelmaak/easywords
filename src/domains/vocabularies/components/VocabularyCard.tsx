26d4660158f9787ab36960402da178ee8e04754b
Daniel Macak <daniel.macak@email.cz>
2024-10-10 16:59:50 +0200

    add: make a button out of vocab card

--- src/domains/vocabularies/components/VocabularyCard.tsx
+++ src/domains/vocabularies/components/VocabularyCard.tsx
@@ -28,67 +28,69 @@ export const VocabularyCard: Component<Props> = props => {
   const wordCount = () => props.wordCount ?? 10;
 
   return (
-    <Card
+    <button
       class={cn('flex flex-col cursor-pointer', props.class)}
       onClick={() => props.onClick?.(props.vocabulary.id)}
     >
-      <CardHeader class="p-4 flex flex-row justify-between items-center gap-4">
-        <CardTitle class="text-md">
-          <span class={`mr-2 fi fi-${props.vocabulary.country}`}></span>
-          {props.vocabulary.name}
-        </CardTitle>
-        <div class="flex gap-4" onClick={e => e.stopPropagation()}>
-          <Show when={props.onDeleteVocabulary}>
-            <HiOutlineTrash
-              class="cursor-pointer"
-              size={16}
-              onClick={() => props.onDeleteVocabulary?.(props.vocabulary.id)}
-            />
-          </Show>
-        </div>
-      </CardHeader>
-      <CardContent class="px-4 pt-0 pb-2 overflow-hidden">
-        <ul class="text-center">
-          <For each={props.vocabulary.words.slice(0, wordCount())}>
-            {item => (
-              <li class="truncate">
-                {item.original} - {item.translation}
-              </li>
-            )}
-          </For>
-          <Show when={props.vocabulary.words.length > wordCount()}>
-            <li class="text-center">...</li>
-          </Show>
-        </ul>
-      </CardContent>
-      <CardFooter class="mt-auto p-4 pt-0">
-        <div class="ml-auto flex gap-2" onClick={e => e.stopPropagation()}>
-          <Show when={props.vocabulary.savedProgress}>
+      <Card>
+        <CardHeader class="p-4 flex flex-row justify-between items-center gap-4">
+          <CardTitle class="text-md">
+            <span class={`mr-2 fi fi-${props.vocabulary.country}`}></span>
+            {props.vocabulary.name}
+          </CardTitle>
+          <div class="flex gap-4" onClick={e => e.stopPropagation()}>
+            <Show when={props.onDeleteVocabulary}>
+              <HiOutlineTrash
+                class="cursor-pointer"
+                size={16}
+                onClick={() => props.onDeleteVocabulary?.(props.vocabulary.id)}
+              />
+            </Show>
+          </div>
+        </CardHeader>
+        <CardContent class="px-4 pt-0 pb-2 overflow-hidden">
+          <ul class="text-center">
+            <For each={props.vocabulary.words.slice(0, wordCount())}>
+              {item => (
+                <li class="truncate">
+                  {item.original} - {item.translation}
+                </li>
+              )}
+            </For>
+            <Show when={props.vocabulary.words.length > wordCount()}>
+              <li class="text-center">...</li>
+            </Show>
+          </ul>
+        </CardContent>
+        <CardFooter class="mt-auto p-4 pt-0">
+          <div class="ml-auto flex gap-2" onClick={e => e.stopPropagation()}>
+            <Show when={props.vocabulary.savedProgress}>
+              <Button
+                size="sm"
+                variant="secondary"
+                onClick={() =>
+                  props.onTestVocabulary(props.vocabulary.id, {
+                    useSavedProgress: true,
+                  })
+                }
+              >
+                <HiOutlineAcademicCap class="mr-1" />
+                Continue Test
+              </Button>
+            </Show>
             <Button
               size="sm"
               variant="secondary"
-              onClick={() =>
-                props.onTestVocabulary(props.vocabulary.id, {
-                  useSavedProgress: true,
-                })
-              }
+              onClick={() => {
+                props.onTestVocabulary(props.vocabulary.id);
+              }}
             >
               <HiOutlineAcademicCap class="mr-1" />
-              Continue Test
+              Test
             </Button>
-          </Show>
-          <Button
-            size="sm"
-            variant="secondary"
-            onClick={() => {
-              props.onTestVocabulary(props.vocabulary.id);
-            }}
-          >
-            <HiOutlineAcademicCap class="mr-1" />
-            Test
-          </Button>
-        </div>
-      </CardFooter>
-    </Card>
+          </div>
+        </CardFooter>
+      </Card>
+    </button>
   );
 };
