1ff0bc1d3a8172865a53f51ef3884c1ca52a2c41
Daniel Macak <daniel.macak@email.cz>
2025-07-20 14:18:48 +0200

    add: make the backlink use back navigation

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -1,4 +1,4 @@
-import { A, useLocation } from '@solidjs/router';
+import { A, useLocation, useNavigate } from '@solidjs/router';
 import { cx } from 'class-variance-authority';
 import { HiOutlineArrowLeft } from 'solid-icons/hi';
 import type { ComponentProps } from 'solid-js';
@@ -11,22 +11,36 @@ type Props = ComponentProps<'a'> & {
 export const BackLink: Component<Props> = props => {
   const [local, rest] = splitProps(props, ['class', 'children', 'backTo']);
   const location = useLocation();
+  const navigate = useNavigate();
 
-  const previousPathname = () =>
-    location.state && 'previous' in location.state
-      ? (location.state.previous as string)
-      : undefined;
-  const href = () => {
-    const prevPathname = previousPathname();
+  const backPath = () => {
+    const prevPathname =
+      location.state && 'previous' in location.state
+        ? (location.state.previous as string)
+        : undefined;
     return local.backTo && prevPathname?.includes(local.backTo)
-      ? prevPathname
-      : '..';
+      ? local.backTo
+      : undefined;
+  };
+
+  const href = () => {
+    return backPath() ?? '..';
+  };
+
+  const goBack = (e: Event) => {
+    e.preventDefault();
+    if (backPath()) {
+      window.history.back();
+    } else {
+      navigate(href());
+    }
   };
 
   return (
     <A
       class={cx(local.class, 'flex items-center text-sm text-inherit')}
       href={href()}
+      onClick={goBack}
       {...rest}
     >
       <HiOutlineArrowLeft class="mr-2" size={16} />
